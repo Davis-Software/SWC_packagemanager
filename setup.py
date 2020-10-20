d03a99721e2552fd4ab67ddbc04a1c07017c43f0
Davis-Software <davissoftware6@gmail.com>
2020-10-20 22:12:49 +0200

    preparing for first package release

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     packages=find_packages(),
     license='MIT',
     description='Manage Software City Packages',
+    download_url='https://github.com/Software-City/SWC_packagemanager/archive/v0.1.tar.gz',
     author="Davis_Software",
     include_package_data=True,
     install_requires=[
