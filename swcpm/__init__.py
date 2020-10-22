965bcf65c75a8dedb21c1e9bf76c30e021edd3f9
Davis-Software <davissoftware6@gmail.com>
2020-10-22 20:34:24 +0200

    more stuff added

--- swcpm/__init__.py
+++ swcpm/__init__.py
@@ -1,10 +1,16 @@
 import click
+import tempfile
+import platform
+
 
 param_verbose = False
 param_old_api = False
 
 constant_projectURL = "https://projects.software-city.org/resources"
 constant_packageURL = "https://projects.software-city.org/resources/swc_packages"
+
+constant_tempPATH = tempfile.gettempdir()
+constant_defaultInstallPATH = "C:\\Program Files" if platform.system() == "win32" else "/usr"
 
 
 @click.group()
--- swcpm/info/info_func.py
+++ swcpm/info/info_func.py
@@ -1,6 +1,18 @@
 import requests
+from click import echo
 from swcpm import constant_packageURL
 
 
+def get_info(package, raw):
+    infoURL = f"{constant_packageURL}/{package}/package.json"
+    resp = requests.get(infoURL)
+    if resp.status_code == 200:
+        data = resp.json()
+        if raw:
+            return data
+        return f"Package ID: {data['id']}\nPackage Type: {data['type']}\nRequired Packages: {data['requirements']}"
+    return "Package doesn't exist"
+
+
 def info_func(package, raw):
-    pass
+    echo(get_info(package, raw))
