5d5a1e0c37311179a5c349aa9132f0b2439fc061
david <davissoftware6@gmail.com>
2021-07-19 17:40:30 +0200

    fixed fatal error again

--- swcpm/__init__.py
+++ swcpm/__init__.py
@@ -1,10 +1,41 @@
 import os
+import json
 import click
 import tempfile
 import platform
 from pathlib import Path
 
-from .tools import loc_db
+
+class LocationDatabase:
+    database_dir = os.path.join(str(Path.home()), "SWC", "database")
+    database_path = os.path.join(database_dir, "locations.json")
+
+    def __init__(self):
+        if not os.path.exists(self.database_path):
+            os.makedirs(self.database_dir, exist_ok=True)
+            with open(self.database_path, "w") as f:
+                f.write("{}")
+
+        with open(self.database_path, "r") as f:
+            self.locations = json.loads(f.read())
+
+    def __update(self):
+        with open(self.database_path, "w") as f:
+            f.write(json.dumps(self.locations))
+
+    def get_location(self, package):
+        try:
+            return self.locations[package]
+        except KeyError:
+            return None
+
+    def set_location(self, package, location):
+        self.locations[package] = location
+        self.__update()
+
+    def remove_location(self, package):
+        del self.locations[package]
+        self.__update()
 
 
 _param_verbose = False
